739ac218dcbd5354a232b1538894e11e3d3e1f60
AlexanderKamarou <127928437+AlexanderKamarou@users.noreply.github.com>
2023-05-10 15:07:03 +0200

    add query for actor_film rent

--- Alexander_Komarov_SQL_queries.sql
+++ Alexander_Komarov_SQL_queries.sql
@@ -1,6 +1,19 @@
+-- Вывести количество фильмов в каждой категории, отсортировать по убыванию.
+
 SELECT c.name AS category_name, COUNT(f.film_id) AS number_of_films
 FROM film_category AS fc
 JOIN film AS f ON f.film_id=fc.film_id
 JOIN category AS c ON c.category_id=fc.category_id
 GROUP BY category_name
 ORDER BY number_of_films DESC;
+
+-- Вывести 10 актеров, чьи фильмы большего всего арендовали, отсортировать по убыванию.
+SELECT a.first_name, a.last_name, COUNT(r.rental_id) AS number_of_rentals
+FROM actor AS a
+JOIN film_actor AS fa ON a.actor_id=fa.actor_id
+JOIN inventory AS i ON fa.film_id=i.film_id
+JOIN rental AS r ON i.inventory_id=r.inventory_id
+GROUP BY a.actor_id
+ORDER BY number_of_rentals DESC
+LIMIT 10;
+
