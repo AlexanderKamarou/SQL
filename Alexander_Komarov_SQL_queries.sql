2aa13184b11994d4c66a4d8055ad0170efab4f90
AlexanderKamarou <127928437+AlexanderKamarou@users.noreply.github.com>
2023-05-10 15:29:51 +0200

    update

--- Alexander_Komarov_SQL_queries.sql
+++ Alexander_Komarov_SQL_queries.sql
@@ -4,7 +4,7 @@ SELECT c.name AS category_name, COUNT(f.film_id) AS number_of_films
 FROM film_category AS fc
 JOIN film AS f ON f.film_id=fc.film_id
 JOIN category AS c ON c.category_id=fc.category_id
-GROUP BY category_name
+GROUP BY c.category_id
 ORDER BY number_of_films DESC;
 
 -- Вывести 10 актеров, чьи фильмы большего всего арендовали, отсортировать по убыванию.
