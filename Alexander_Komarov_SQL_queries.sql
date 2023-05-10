cd361734231e007446027073d40203b8c8f7fc71
AlexanderKamarou <127928437+AlexanderKamarou@users.noreply.github.com>
2023-05-10 15:27:19 +0200

    Update Alexander_Komarov_SQL_queries.sql
    add most expemsive category

--- Alexander_Komarov_SQL_queries.sql
+++ Alexander_Komarov_SQL_queries.sql
@@ -8,6 +8,7 @@ GROUP BY category_name
 ORDER BY number_of_films DESC;
 
 -- Вывести 10 актеров, чьи фильмы большего всего арендовали, отсортировать по убыванию.
+
 SELECT a.first_name, a.last_name, COUNT(r.rental_id) AS number_of_rentals
 FROM actor AS a
 JOIN film_actor AS fa ON a.actor_id=fa.actor_id
@@ -16,4 +17,17 @@ JOIN rental AS r ON i.inventory_id=r.inventory_id
 GROUP BY a.actor_id
 ORDER BY number_of_rentals DESC
 LIMIT 10;
+
+-- Вывести категорию фильмов, на которую потратили больше всего денег.
+
+SELECT c.name AS category_name, SUM(p.amount) AS spent
+postgres-# FROM category AS c
+postgres-# JOIN film_category AS fc ON c.category_id=fc.category_id
+postgres-# JOIN film AS f ON f.film_id=fc.film_id
+postgres-# JOIN inventory AS i ON f.film_id=i.film_id
+postgres-# JOIN rental AS r ON i.inventory_id=r.inventory_id
+postgres-# JOIN payment AS p ON r.rental_id=p.rental_id
+postgres-# GROUP BY c.category_id
+postgres-# ORDER BY spent DESC
+postgres-# LIMIT 1;
 
